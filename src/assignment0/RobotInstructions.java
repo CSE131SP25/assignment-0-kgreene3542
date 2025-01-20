d30c55494e4bdb559fdbae4edf0ea54fe71b4a03
k.agreene <k.agreene@localhost>
2025-01-20 13:58:33 -0600

    Made a wonky star with the robot.

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,32 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
-		robot.turnLeft(90);
-		robot.forward(23);
-		robot.turnRight(90);
+		robot.forward(100);
+		robot.turnLeft(120);
 		robot.forward(80);
+		robot.turnRight(160);
+		robot.forward(90);
+		robot.turnLeft(90);
+		robot.forward(100);
+		robot.turnRight(160);
+		robot.forward(100);
+		robot.turnLeft(90);
+		robot.forward(100);
+		robot.turnRight(160);
+		robot.forward(100);
+		robot.turnLeft(120);
+		robot.forward(100);
+		robot.turnRight(160);
+		robot.forward(120);
+		robot.turnLeft(80);
+		robot.forward(100);
+		robot.turnRight(160);
+		robot.forward(140);
+		
+		
+		
+		
+		
 	}
 
 }
